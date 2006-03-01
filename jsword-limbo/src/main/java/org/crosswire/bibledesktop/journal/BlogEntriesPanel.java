54af4a621582fe5bd8f6bd311880739939f2a151
DM Smith <dmsmith@crosswire.org>
2006-03-01 15:55:18 +0000

    temporarily moved blogging out of bibledesktop

---
+++ jsword-limbo/src/main/java/org/crosswire/bibledesktop/journal/BlogEntriesPanel.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2005, Dave Johnson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.crosswire.bibledesktop.journal;
+
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.crosswire.bibledesktop.signal.LoadEntrySignal;
+import org.werx.framework.bus.ReflectionBus;
+
+import com.manning.blogapps.chapter08.blogclient.Blog;
+
+/**
+ * Panel with table that lists recent blog entries.
+ * @author David M Johnson
+ */
+public class BlogEntriesPanel extends JPanel implements BlogClientTab
+{
+    /**
+     * Creates new form BlogEntriesPanel
+     */
+    public BlogEntriesPanel()
+    {
+        initComponents();
+        mBlogEntries.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                if (e.getClickCount() == 2)
+                {
+                    int row = mBlogEntries.rowAtPoint(e.getPoint());
+                    TableModel model = mBlogEntries.getModel();
+                    String id = (String) model.getValueAt(row, ID_COLUMN);
+                    ReflectionBus.broadcast(new LoadEntrySignal(blogSite, id));
+                }
+            }
+        });
+    }
+
+    /**
+     * This method is called from within the constructor to
+     * initialize the form.
+     */
+    private void initComponents()
+    {
+        mScrollPane = new JScrollPane();
+        mBlogEntries = new JTable();
+
+        setLayout(new BorderLayout());
+
+        setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+        mScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        mScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        mBlogEntries.setModel(new DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                Msg.ENTRY_TITLE.toString(new Integer(1)),
+                Msg.ENTRY_TITLE.toString(new Integer(2)),
+                Msg.ENTRY_TITLE.toString(new Integer(3)),
+                Msg.ENTRY_TITLE.toString(new Integer(4)),
+            }
+        ));
+        mScrollPane.setViewportView(mBlogEntries);
+
+        add(mScrollPane, BorderLayout.NORTH);
+    }
+
+    /**
+     * Inject BlogSite dependency.
+     */
+    public void setBlog(Blog blogSite)
+    {
+        this.blogSite = blogSite;
+        reset();
+    }
+
+    /**
+     * Update table of blog entries.
+     */
+    public void onSelected()
+    {
+        reset();
+    }
+
+    public void reset()
+    {
+        AbstractTableModel model = new BlogEntriesTableModel(blogSite);
+        mBlogEntries.setModel(model);
+        model.fireTableDataChanged();
+    }
+
+    JTable mBlogEntries;
+    private JScrollPane mScrollPane;
+
+    Blog blogSite;
+    public static final int TITLE_COLUMN = 0;
+    public static final int DATE_COLUMN = 1;
+    public static final int ID_COLUMN = 2;
+    public static final int COLUMN_COUNT = 3;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ jsword-limbo/src/main/java/org/crosswire/bibledesktop/journal/Msg.java
@@ -0,0 +1,75 @@
+/**
+ * Distribution License:
+ * BibleDesktop is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2 as published by
+ * the Free Software Foundation. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * The License is available on the internet at:
+ *       http://www.gnu.org/copyleft/gpl.html
+ * or by writing to:
+ *      Free Software Foundation, Inc.
+ *      59 Temple Place - Suite 330
+ *      Boston, MA 02111-1307, USA
+ *
+ * Copyright: 2005
+ *     The copyright to this program is held by it's authors.
+ *
+ * ID: $Id: Msg.java 758 2005-06-22 03:50:55Z dmsmith $
+ */
+package org.crosswire.bibledesktop.journal;
+
+import org.crosswire.common.util.MsgBase;
+
+/**
+ * Compile safe Msg resource settings.
+ * 
+ * @see gnu.gpl.License for license details.
+ *      The copyright to this program is held by it's authors.
+ * @author DM Smith [dmsmith555 at yahoo dot com]
+ */
+final class Msg extends MsgBase
+{
+    static final Msg BLOG_TITLE = new Msg("BlogClientFrame.BlogTitle"); //$NON-NLS-1$
+    static final Msg START_BLOG = new Msg("BlogClientFrame.StartBlog"); //$NON-NLS-1$
+    static final Msg ACCOUNT_NAME = new Msg("BlogClientFrame.AccountName"); //$NON-NLS-1$
+    static final Msg PASSWORD = new Msg("BlogClientFrame.Password"); //$NON-NLS-1$
+    // I18N: migrate this to an ActionFactory
+    static final Msg SUBMIT = new Msg("BlogClientFrame.Submit"); //$NON-NLS-1$
+    static final Msg EDIT_ENTRY = new Msg("BlogClientFrame.EditEntry"); //$NON-NLS-1$
+    static final Msg ALL_ENTRIES = new Msg("BlogClientFrame.AllEntries"); //$NON-NLS-1$
+    static final Msg CONNECTING = new Msg("BlogClientFrame.Connecting"); //$NON-NLS-1$
+    static final Msg CONNECTED = new Msg("BlogClientFrame.Connected"); //$NON-NLS-1$
+    static final Msg JOURNAL_RECEIVED = new Msg("BlogClientFrame.JournalReceived"); //$NON-NLS-1$
+    static final Msg ALL_DONE = new Msg("BlogClientFrame.AllDone"); //$NON-NLS-1$
+    static final Msg MORE = new Msg("BlogClientFrame.More"); //$NON-NLS-1$
+    static final Msg NO_JOURNALS = new Msg("BlogClientFrame.NoJournals"); //$NON-NLS-1$
+    static final Msg CANNOT_CONNECT = new Msg("BlogClientFrame.CannotConnect"); //$NON-NLS-1$
+
+    static final Msg DELETE_ERROR = new Msg("BlogClientPanel.DeleteError"); //$NON-NLS-1$
+    static final Msg UPLOAD_ERROR = new Msg("BlogClientPanel.UploadError"); //$NON-NLS-1$
+    static final Msg SAVE_SUCCESS = new Msg("BlogClientPanel.SaveSuccess"); //$NON-NLS-1$
+    static final Msg MISSING_CONTENT = new Msg("BlogClientPanel.MissingContent"); //$NON-NLS-1$
+    static final Msg TITLE_ENTRY = new Msg("BlogClientPanel.TitleEntry"); //$NON-NLS-1$
+    static final Msg CATEGORY_ENTRY = new Msg("BlogClientPanel.CategoryEntry"); //$NON-NLS-1$
+    static final Msg PUBLISH = new Msg("BlogClientPanel.Publish"); //$NON-NLS-1$
+    static final Msg NEW = new Msg("BlogClientPanel.New"); //$NON-NLS-1$
+    static final Msg SAVE_DRAFT = new Msg("BlogClientPanel.SaveDraft"); //$NON-NLS-1$
+    static final Msg DELETE = new Msg("BlogClientPanel.Delete"); //$NON-NLS-1$
+
+    static final Msg ENTRY_TITLE = new Msg("BlogEntriesPanel.EntryTitle"); //$NON-NLS-1$
+
+    static final Msg TITLE_COLUMN = new Msg("BlogEntriesPanel.TitleColumn"); //$NON-NLS-1$
+    static final Msg ID_COLUMN = new Msg("BlogEntriesPanel.IdColumn"); //$NON-NLS-1$
+    static final Msg DATE_COLUMN = new Msg("BlogEntriesPanel.DateColumn"); //$NON-NLS-1$
+
+    /**
+     * Passthrough ctor
+     */
+    private Msg(String name)
+    {
+        super(name);
+    }
+}
