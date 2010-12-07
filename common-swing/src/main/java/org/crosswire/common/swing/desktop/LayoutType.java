f6de17c97c1dc4c92b2f1305d6588845a7188c96
DM Smith <dmsmith@crosswire.org>
2010-12-07 03:49:58 +0000

    JS-141: Converted JSword's enums to be Java 5 enums

--- common-swing/src/main/java/org/crosswire/common/swing/desktop/LayoutType.java
+++ common-swing/src/main/java/org/crosswire/common/swing/desktop/LayoutType.java
@@ -21,7 +21,6 @@
  */
 package org.crosswire.common.swing.desktop;
 
-import java.io.Serializable;
 
 /**
  * Types of ViewLayouts. Currently there are two types of desktop layouts:
@@ -35,75 +34,36 @@ import java.io.Serializable;
  * @author Joe Walker [joe at eireneh dot com]
  * @author DM Smith [dmsmith555 at yahoo dot com]
  */
-public abstract class LayoutType implements Serializable {
+public enum LayoutType {
     /**
      * Tabbed View
      */
-    public static final LayoutType TDI = new LayoutType("TDI")
-    {
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.crosswire.common.swing.desktop.LayoutType#createLayout()
-         */
+    TDI {
         public AbstractViewLayout createLayout() {
             return new TDIViewLayout();
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.crosswire.common.swing.desktop.LayoutType#toString()
-         */
         public String toString() {
             // TRANSLATOR: This is the name of one of two different ways to present Bible Views.
             // These show up in Options/Preferences.
             return UserMsg.gettext("Tabbed Document Interface");
         }
-
-        /**
-         * Serialization ID
-         */
-        private static final long serialVersionUID = 3257572784669800241L;
-    };
+    },
 
     /**
      * Multiple Document View
      */
-    public static final LayoutType MDI = new LayoutType("MDI")
-    {
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.crosswire.common.swing.desktop.LayoutType#createLayout()
-         */
+    MDI {
         public AbstractViewLayout createLayout() {
             return new MDIViewLayout();
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.crosswire.common.swing.desktop.LayoutType#toString()
-         */
         public String toString() {
             // TRANSLATOR: This is the name of one of two different ways to present Bible Views.
             // These show up in Options/Preferences.
             return UserMsg.gettext("Multiple Document Interface");
         }
-
-        /**
-         * Serialization ID
-         */
-        private static final long serialVersionUID = 3904681587101874488L;
     };
-
-    /**
-     * Simple ctor
-     */
-    protected LayoutType(String name) {
-        this.name = name;
-    }
 
     /**
      * Return the layout
@@ -131,91 +91,41 @@ public abstract class LayoutType implements Serializable {
      * Get an integer representation for this LayoutType
      */
     public int toInteger() {
-        for (int i = 0; i < VALUES.length; i++) {
-            if (equals(VALUES[i])) {
-                return i;
-            }
-        }
-        // cannot get here
-        assert false;
-        return -1;
+        return ordinal();
     }
 
     /**
      * Lookup method to convert from a String
      */
     public static LayoutType fromString(String name) {
-        for (int i = 0; i < VALUES.length; i++) {
-            LayoutType obj = VALUES[i];
-            if (obj.name.equalsIgnoreCase(name)) {
-                return obj;
+        for (LayoutType v : values()) {
+            if (v.name().equalsIgnoreCase(name)) {
+                return v;
             }
         }
+
         // cannot get here
         assert false;
         return null;
     }
 
     /**
-     * Lookup method to convert from an integer
+     * Lookup method by ordinal value
      */
-    public static LayoutType fromInteger(int i) {
-        return VALUES[i];
-    }
+    public static LayoutType fromInteger(int ordinal) {
+        for (LayoutType v : values()) {
+            if (v.ordinal() == ordinal) {
+                return v;
+            }
+        }
 
-    /**
-     * Prevent subclasses from overriding canonical identity based Object
-     * methods
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    public final boolean equals(Object o) {
-        return super.equals(o);
+        // cannot get here
+        assert false;
+        return null;
     }
-
-    /**
-     * Prevent subclasses from overriding canonical identity based Object
-     * methods
-     * 
-     * @see java.lang.Object#hashCode()
-     */
-    public final int hashCode() {
-        return super.hashCode();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#toString()
-     */
-    public String toString() {
-        return name;
-    }
-
-    /**
-     * The name of the LayoutType
-     */
-    private String name;
 
     /**
      * The actual layout
      */
-    protected AbstractViewLayout layout;
-
-    // Support for serialization
-    private static int nextObj;
-    private final int obj = nextObj++;
-
-    Object readResolve() {
-        return VALUES[obj];
-    }
-
-    private static final LayoutType[] VALUES = {
-            TDI, MDI,
-    };
-
-    /**
-     * Serialization ID
-     */
-    private static final long serialVersionUID = 5443546249176482187L;
+    private transient AbstractViewLayout layout;
 }
